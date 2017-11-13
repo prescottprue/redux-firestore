4c8533df0d0c0a71440c6c716417044af1e5d2d1
prescottprue <sprue.dev@gmail.com>
2017-11-13 02:41:39 -0800

    Subcollection data now correctly appearing in ordered.

--- src/reducers/orderedReducer.js
+++ src/reducers/orderedReducer.js
@@ -1,7 +1,29 @@
-import { pick } from 'lodash';
+import { pick, get, first } from 'lodash';
 import { actionTypes } from '../constants';
 
 const { GET_SUCCESS, LISTENER_RESPONSE, CLEAR_DATA } = actionTypes;
+
+function updateObject(oldObject, newValues) {
+    // Encapsulate the idea of passing a new object as the first parameter
+    // to Object.assign to ensure we correctly copy data instead of mutating
+  return Object.assign({}, oldObject, newValues);
+}
+
+function updateItemInArray(array, itemId, updateItemCallback) {
+  const updatedItems = array.map((item) => {
+    if (item.id !== itemId) {
+            // Since we only want to update one item, preserve all others as they are now
+      return item;
+    }
+
+        // Use the provided callback to create an updated item
+    const updatedItem = updateItemCallback(item);
+    return updatedItem;
+  });
+
+  return updatedItems;
+}
+
 
 /**
  * Reducer for ordered state.
@@ -19,6 +41,19 @@ export default function orderedReducer(state = {}, action) {
         return state;
       }
       // TODO: Support merging
+      if (action.meta.doc) {
+        const itemToAdd = first(action.payload.ordered);
+        const subcollection = first(action.meta.subcollections);
+        // TODO: Make this recursive so that is supports multiple subcollections
+        return {
+          ...state,
+          [action.meta.collection]: updateItemInArray(
+            state[action.meta.collection] || [],
+            action.meta.doc,
+            item => updateObject(item, subcollection ? { [get(subcollection, 'collection')]: action.payload.ordered } : itemToAdd),
+          ),
+        };
+      }
       return {
         ...state,
         [action.meta.collection]: action.payload.ordered,
