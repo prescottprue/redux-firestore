4c998f8cdc6a46e7d068501ba196089c48987a52
prescottprue <sprue.dev@gmail.com>
2017-12-16 16:41:42 -0800

    Replaced add-module-exports with using export directly (to prevent issue with transform-inline-environment-variables)

--- src/index.js
+++ src/index.js
@@ -5,14 +5,32 @@ import createFirestoreInstance from './createFirestoreInstance';
 import constants, { actionTypes } from './constants';
 import middleware, { CALL_FIRESTORE } from './middleware';
 
+// converted with transform-inline-environment-variables
+const version = process.env.npm_package_version;
+
+export {
+  version,
+  reducer,
+  reducer as firestoreReducer,
+  enhancer,
+  enhancer as reduxFirestore,
+  createFirestoreInstance,
+  firestoreActions as actions,
+  getFirestore,
+  constants,
+  actionTypes,
+  middleware,
+  CALL_FIRESTORE,
+};
+
 export default {
-  version: process.env.npm_package_version, // converted with transform-inline-environment-variables
+  version,
+  reducer,
   firestoreReducer: reducer,
+  enhancer,
   reduxFirestore: enhancer,
   createFirestoreInstance,
   actions: firestoreActions,
-  reducer,
-  enhancer,
   getFirestore,
   constants,
   actionTypes,
