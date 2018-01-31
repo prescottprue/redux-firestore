39f6f3f0b0f9436bb3a9936743f9acb794bd8ac2
danleavitt0 <danleavitt0@gmail.com>
2018-01-30 16:25:22 -0800

    spec

--- test/unit/utils/query.spec.js
+++ test/unit/utils/query.spec.js
@@ -1,8 +1,4 @@
-import {
-  attachListener,
-  getQueryConfigs,
-  firestoreRef,
-} from '../../../src/utils/query';
+import { attachListener, getQueryConfigs, firestoreRef } from '../../../src/utils/query';
 
 let dispatch;
 let meta;
@@ -51,13 +47,15 @@ describe('query utils', () => {
     });
 
     it('throws if meta is not included', () => {
-      expect(() => attachListener({}, dispatch))
-        .to.Throw('Meta data is required to attach listener.');
+      expect(() => attachListener({}, dispatch)).to.Throw(
+        'Meta data is required to attach listener.',
+      );
     });
 
     it('throws if _ variable is not defined on Firebase', () => {
-      expect(() => attachListener({}, dispatch, { collection: 'test' }))
-        .to.Throw('Internal Firebase object required to attach listener. Confirm that reduxFirestore enhancer was added when you were creating your store');
+      expect(() => attachListener({}, dispatch, { collection: 'test' })).to.Throw(
+        'Internal Firebase object required to attach listener. Confirm that reduxFirestore enhancer was added when you were creating your store',
+      );
     });
   });
 
@@ -67,47 +65,48 @@ describe('query utils', () => {
     });
 
     it('it throws for invalid input', () => {
-      expect(() => getQueryConfigs(1))
-        .to.Throw('Querie(s) must be an Array or a string');
+      expect(() => getQueryConfigs(1)).to.Throw('Querie(s) must be an Array or a string');
     });
 
     describe('array', () => {
       it('with collection in string', () => {
-        expect(getQueryConfigs(['test']))
-          .to.have.nested.property('0.collection', 'test');
+        expect(getQueryConfigs(['test'])).to.have.nested.property('0.collection', 'test');
       });
 
       it('with collection in an object', () => {
-        expect(getQueryConfigs([{ collection: 'test' }]))
-          .to.have.nested.property('0.collection', 'test');
+        expect(getQueryConfigs([{ collection: 'test' }])).to.have.nested.property(
+          '0.collection',
+          'test',
+        );
       });
 
       it('with collection and doc in an object', () => {
         meta = [{ collection: 'test', doc: 'other' }];
         result = getQueryConfigs(meta);
-        expect(result)
-          .to.have.nested.property('0.collection', meta[0].collection);
+        expect(result).to.have.nested.property('0.collection', meta[0].collection);
         expect(result).to.have.nested.property('0.doc', meta[0].doc);
       });
 
       it('throws invalid object', () => {
         meta = [{ test: 'test' }];
-        expect(() => getQueryConfigs(meta))
-          .to.Throw('Collection and/or Doc are required parameters within query definition object');
+        expect(() => getQueryConfigs(meta)).to.Throw(
+          'Collection and/or Doc are required parameters within query definition object',
+        );
       });
     });
 
     describe('string', () => {
       it('with collection', () => {
-        expect(getQueryConfigs('test'))
-          .to.have.property('collection', 'test');
+        expect(getQueryConfigs('test')).to.have.property('collection', 'test');
       });
     });
 
     describe('object', () => {
       it('with collection', () => {
-        expect(getQueryConfigs({ collection: 'test' }))
-          .to.have.nested.property('0.collection', 'test');
+        expect(getQueryConfigs({ collection: 'test' })).to.have.nested.property(
+          '0.collection',
+          'test',
+        );
       });
 
       it('with doc', () => {
@@ -122,7 +121,10 @@ describe('query utils', () => {
         result = getQueryConfigs(meta);
         expect(result).to.have.nested.property('0.collection', meta.collection);
         expect(result).to.have.nested.property('0.doc', meta.doc);
-        expect(result).to.have.nested.property('0.subcollections.0.collection', meta.subcollections[0].collection);
+        expect(result).to.have.nested.property(
+          '0.subcollections.0.collection',
+          meta.subcollections[0].collection,
+        );
       });
     });
   });
@@ -135,7 +137,7 @@ describe('query utils', () => {
     describe('doc', () => {
       it('creates ref', () => {
         meta = { collection: 'test', doc: 'other' };
-        const docSpy = sinon.spy(() => ({ }));
+        const docSpy = sinon.spy(() => ({}));
         const fakeFirebase = { firestore: () => ({ collection: () => ({ doc: docSpy }) }) };
         result = firestoreRef(fakeFirebase, dispatch, meta);
         expect(result).to.be.an('object');
@@ -146,7 +148,7 @@ describe('query utils', () => {
     describe('subcollections', () => {
       it('creates ref with collection', () => {
         meta = { collection: 'test', doc: 'other', subcollections: [{ collection: 'thing' }] };
-        const docSpy = sinon.spy(() => ({ }));
+        const docSpy = sinon.spy(() => ({}));
         const fakeFirebase = {
           firestore: () => ({
             collection: () => ({
@@ -162,8 +164,12 @@ describe('query utils', () => {
       });
 
       it('creates ref with doc', () => {
-        meta = { collection: 'test', doc: 'other', subcollections: [{ collection: 'thing', doc: 'again' }] };
-        const docSpy = sinon.spy(() => ({ }));
+        meta = {
+          collection: 'test',
+          doc: 'other',
+          subcollections: [{ collection: 'thing', doc: 'again' }],
+        };
+        const docSpy = sinon.spy(() => ({}));
         const fakeFirebase = {
           firestore: () => ({
             collection: () => ({
