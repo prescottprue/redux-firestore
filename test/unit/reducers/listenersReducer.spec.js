c543afae01569dbef59532b61b98c641b710b1b5
Scott Prue <scott@residenetwork.com>
2018-02-08 23:46:52 -0800

    More tests added for listenersReducer and errorsReducer

--- test/unit/reducers/listenersReducer.spec.js
+++ test/unit/reducers/listenersReducer.spec.js
@@ -1,4 +1,8 @@
 import listenersReducer from '../../../src/reducers/listenersReducer';
+import { actionTypes } from '../../../src/constants';
+
+let state;
+let action;
 
 describe('listenersReducer', () => {
   it('is exported', () => {
@@ -9,5 +13,34 @@ describe('listenersReducer', () => {
   });
   it('returns state for undefined actionType', () => {
     expect(listenersReducer({}, {})).to.exist;
+  });
+  it('exports both byId and allIds state', () => {
+    const result = listenersReducer({}, {});
+    expect(result).to.have.property('byId');
+    expect(result).to.have.property('allIds');
+  });
+  describe('allIds sub-reducer', () => {
+    describe('actionTypes', () => {
+      describe('LISTENER_ERROR', () => {
+        it('returns state if payload is not defined', () => {
+          action = {
+            meta: { collection: 'test' },
+            type: actionTypes.LISTENER_ERROR,
+          };
+          state = { allIds: [], byId: {} };
+          expect(listenersReducer(state, action)).to.be.an('object');
+        });
+
+        it('returns state if payload is not defined', () => {
+          action = {
+            meta: { collection: 'test' },
+            type: actionTypes.LISTENER_ERROR,
+            payload: { some: 'data' },
+          };
+          state = { allIds: [], byId: {} };
+          expect(listenersReducer(state, action)).to.be.an('object');
+        });
+      });
+    });
   });
 });
