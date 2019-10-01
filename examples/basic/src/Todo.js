e71fb25253e958a61c3df6e792fd765aeeee353b
Scott Prue <scott@residebrokerage.com>
2019-10-01 00:47:24 -0600

    Update basic example

--- examples/basic/src/Todo.js
+++ examples/basic/src/Todo.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types'
-import { compose, flattenProp, withHandlers, pure } from 'recompose'
 import { withFirestore } from './utils';
 
 const styles = {
@@ -19,51 +18,36 @@ const styles = {
   }
 }
 
-const Todo = ({
-  text,
-  owner,
-  done,
-  disabled,
-  onDoneClick
-}) => (
-  <div style={styles.container}>
-    <input
-      checked={done}
-      onChange={onDoneClick}
-      disabled={disabled}
-      type="checkbox"
-    />
-    <div style={styles.meta}>
-      <span>{text}</span>
-      <span style={styles.owner}>{owner}</span>
+function Todo({ todo, firestore }) {
+  function onDoneClick() {
+    return firestore.update(`todos/${todo.id}`, { done: !todo.done })
+  }
+  return (
+    <div style={styles.container}>
+      <input
+        checked={todo.done}
+        onChange={onDoneClick}
+        disabled={todo.disabled}
+        type="checkbox"
+      />
+      <div style={styles.meta}>
+        <span>{todo.text}</span>
+        <span style={styles.owner}>{todo.owner}</span>
+      </div>
     </div>
-  </div>
-);
+  );
+}
 
 Todo.propTypes = {
-  text: PropTypes.string, // from enhancer (flattenProp)
-  owner: PropTypes.string, // from enhancer (flattenProp)
-  done: PropTypes.bool, // from enhancer (flattenProp)
-  disabled: PropTypes.bool, // from enhancer (flattenProp)
-  onDoneClick: PropTypes.func.isRequired, // from enhancer (withHandlers)
+  todo: PropTypes.shape({
+    text: PropTypes.string,
+    owner: PropTypes.string,
+    done: PropTypes.bool,
+    disabled: PropTypes.bool,
+  }),
   firestore: PropTypes.shape({
     update: PropTypes.func.isRequired
   })
 }
 
-const enhance = compose(
-  // Add props.firestore
-  withFirestore,
-  // Flatten todo prop (creates id, text, owner, done and disabled props)
-  flattenProp('todo'),
-  // Handlers as props
-  withHandlers({
-    onDoneClick: props => () => {
-      return props.firestore.update(`todos/${props.id}`, { done: !props.done })
-    }
-  }),
-  // Prevent unnessesary re-renders by doing shallow comparison of props
-  pure
-)
-
-export default enhance(Todo)
+export default withFirestore(Todo)
