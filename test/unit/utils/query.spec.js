984394d0117ae2dcba0d5f69da2bfef2bfb5970d
Witsarut Sawetkanit <illumina-ova@live.com>
2019-12-20 17:43:18 +0700

    Lint

--- test/unit/utils/query.spec.js
+++ test/unit/utils/query.spec.js
@@ -878,7 +878,7 @@ describe('query utils', () => {
       const fakeSnap = { forEach: docArray.forEach.bind(docArray) };
       result = orderedFromSnap(fakeSnap);
       expect(getSnapshotByObject(result)).to.equal(fakeSnap);
-      expect(getSnapshotByObject(result[0])).to.equal(fakeDocSnap)
+      expect(getSnapshotByObject(result[0])).to.equal(fakeDocSnap);
     });
   });
 });
