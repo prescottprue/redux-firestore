cf6ee6cc14d5ed7ef3dc1251fac25a7aa2f9ff24
Scott Prue <sprue.dev@gmail.com>
2022-01-16 12:58:22 -0700

    chore(dev-deps): update dev deps including babel, sinon and webpack-cli

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,42 @@
 module.exports = {
-	root: true,
-	parser: 'babel-eslint',
-	'extends': [
-		'airbnb-base',
-		'google',
-		'prettier',
-		'plugin:jsdoc/recommended'
-	],
-	plugins: [
-		'babel',
-		'prettier',
-		'jsdoc'
-	],
-	env: {
-		browser: true,
-		es6: true,
-		node: true
-	},
-	settings: {
+  root: true,
+  parser: 'babel-eslint',
+  extends: [
+    'airbnb-base',
+    'google',
+    'prettier',
+    'plugin:jsdoc/recommended'
+  ],
+  plugins: [
+    'babel',
+    'prettier',
+    'jsdoc'
+  ],
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  settings: {
     'import/resolver': {
       node: {
         moduleDirectory: ['node_modules', '/']
       }
-		}
-	},
-	rules: {
-		'prettier/prettier': ['error', {
+    }
+  },
+  rules: {
+    'prettier/prettier': ['error', {
       'singleQuote': true,
       'trailingComma': 'all'
     }],
-		'no-console': 'error',
-		'valid-jsdoc': 0, // from google
-		'no-confusing-arrow': 0,
-		'no-case-declarations': 0,
-		'arrow-parens': [2, 'as-needed'],
-		'prefer-default-export': 0,
-		'jsdoc/newline-after-description': 0,
-		'jsdoc/no-undefined-types': [1, { definedTypes: ['firebase'] }]
-	}
+    'no-console': 'error',
+    'valid-jsdoc': 0, // from google
+    'no-confusing-arrow': 0,
+    'no-case-declarations': 0,
+    'arrow-parens': [2, 'as-needed'],
+    'prefer-default-export': 0,
+    'jsdoc/newline-after-description': 0,
+    'jsdoc/empty-tags': 0, // still add docs functions marked private
+    'jsdoc/no-undefined-types': [1, { definedTypes: ['firebase'] }]
+  }
 }
